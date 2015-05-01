e5b1d2c9e0f8b6b46fdd66438eaa43b6259fa277
hobuobi <obuobhum@gmail.com>
2015-05-01 03:19:42 -0400

    Update Dawg_new.py

--- Dawg_new.py
+++ Dawg_new.py
@@ -35,8 +35,8 @@ class DNode:
 		self.branches_from = []
 		self.final = True
 		
-	def setFinal():
-		self.final = True
+	def setFinal(self,x):
+		self.final = x
 	
 	def childCheck(xNode, yNode):
 		if (len(xNode.branches_to) != len(yNode.branches_to)):
@@ -48,6 +48,7 @@ class DNode:
 					childCheck((xNode.branches_to)[j].getTNode(),(yNode.branches_to)[j].getTNode())
 				else:
 					return False
+				j+=1
 		return True
 				
 	def insert (self,word):
@@ -56,17 +57,20 @@ class DNode:
 		index = 0
 		currentNode = self
 		while (index<len(word)):
-			exist = false
+			print('phase1')
+			exist = False
+			##bran = Branch('',DNode([],[]),DNode([],[])) 
 			for b in currentNode.branches_to:
+				print('branches_to')
 				if b.chr == word[index]:
-					currentNode = b.node
-					exist = true
+					currentNode = b.t_node
+					exist = True
 					index+= 1
 					break
-					
+				
 			if (not exist):
-				newNode = DNode()
-				newBranch = Branch(b.chr,currentNode,newNode)
+				newNode = DNode([],[])
+				newBranch = Branch(word[index],currentNode,newNode)
 				(newNode.branches_from).append(newBranch)
 				currentNode = newNode
 			if(index == len(word)-1):
@@ -74,27 +78,30 @@ class DNode:
 		# Phase 2 - collapse
 		
 		for x in terminalNodes:
-			if(childCheck(currentNode,x)): # childCheck - TODO
+			if(childCheck(currentNode,x)): 
+				print('phase2')
 				if(currentNode.branches_from != []) and (x.branches_from != []) :
 					(currentNode.branches_from)[0].setTNode(x)
-				else
-					####
+
 							
-	def search (word):
+	def search (self,word):
+		index = 0
 		currentNode = self
 		while (index<len(word)):
-			exist = false
+			print('poo1')
+			exist = False
 			nextChar = ''
 			for b in currentNode.branches_to:
 				if b.chr == word[index]:
 					nextChar = b.chr
 					currentNode = b.node
-					exist = true
-					index+= 1
-					break
-			if(index == len(word)):
-				print('')
-			elif(not exist):
-				print('Not found.')
-			else:
-				print(search+nextChar)			
+					exist = True
+			index+= 1
+			break
+		if(index == len(word)):
+			print(index)
+			print(len(word))
+		elif(not exist):
+			print('Not found.')
+		else:
+			print(search+nextChar)			
