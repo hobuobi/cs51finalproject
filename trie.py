fe7c3c4fe6ef7020f806856beab889ec32ca3d92
lzboi225 <larryzhang101@gmail.com>
2015-05-01 03:22:07 -0400

    Modified spaces

--- trie.py
+++ trie.py
@@ -3,22 +3,22 @@ _stop = 'zz'
 class trie(object):
 
     def __init__(self, vocab):
-	self.t = dict()
-	for word in vocab:
-		current = self.t
-		for letter in word:
-			current = current.setdefault(letter.lower(), {})
-		current = current.setdefault(_stop, _stop)
+				self.t = dict()
+				for word in vocab:
+						current = self.t
+				for letter in word:
+						current = current.setdefault(letter.lower(), {})
+				current = current.setdefault(_stop, _stop)
 
     def lookup(self, word):
-	"""Returns True if word is in t, false if not
-	Precondition t is a trie
-	word is a string"""
+				"""Returns True if word is in t, false if not
+				Precondition t is a trie
+				word is a string"""
 
-	letter = word[0:1]
-	if (len(word) == 0) and (_stop in self.t):
-		return True
-	elif letter in self.t:
-		return lookup(self.t[letter], word[1:])
-	else:
-		return False
+				letter = word[0:1]
+				if (len(word) == 0) and (_stop in self.t):
+						return True
+				elif letter in self.t:
+						return lookup(self.t[letter], word[1:])
+				else:
+						return False
