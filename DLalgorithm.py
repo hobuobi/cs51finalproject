d708c8d31170d14ce230972ce9043ee8d3093ff1
Ben Zheng <benjaminzheng@college.harvard.edu>
2015-05-01 00:09:48 -0400

    Moved trie stuff to DLalgorithm.py

--- DLalgorithm.py
+++ DLalgorithm.py
@@ -29,6 +29,30 @@ class hashtable(object):
         random.seed(word)
         return (word in self.hashed[random.randint(0,49)])
 
+_stop = 'zz'
+
+class trie(object):
+
+    def __init__(self, vocab):
+        self.t = dict()
+        for word in vocab:
+            current = self.t
+            for letter in word:
+                current = current.setdefault(letter.lower(), {})
+            current = current.setdefault(_stop,_stop)
+
+    def lookup(self, word):
+        """Returns True if word is in t, false if not
+        Precondition t is a trie word is a string"""
+
+        letter = word[0:1]
+        if (len(word) == 0) and (_stop in self.t):
+            return True
+        elif letter in self.t:
+            return lookup(self.t[letter], word[1:])
+        else:
+            return False
+
 class dl_algorithm(object):
 
     def __init__(self, dictname, structtype):
--- trie.py
+++ trie.py
@@ -1,28 +1,24 @@
 _stop = 'zz'
 
-def trie(vocab):
-	d = dict()
+class trie(object):
+
+    def __init__(self, vocab):
+	self.t = dict()
 	for word in vocab:
-		current = d
+		current = self.t
 		for letter in word:
 			current = current.setdefault(letter.lower(), {})
 		current = current.setdefault(_stop, _stop)
-	return d
 
-_trie1 = trie(open('words.txt'))
-
-def search(t, word):
+    def lookup(self, word):
 	"""Returns True if word is in t, false if not
 	Precondition t is a trie
 	word is a string"""
 
 	letter = word[0:1]
-	if (len(word) == 0) and (_stop in t):
+	if (len(word) == 0) and (_stop in self.t):
 		return True
-	elif letter in t:
-		return True and search(t[letter], word[1:])
+	elif letter in self.t:
+		return lookup(self.t[letter], word[1:])
 	else:
 		return False
-
-def lookup(word):
-	search(_trie1, word)
